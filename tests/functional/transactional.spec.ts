74dcf891ac6bc2fdf5e5e56f03b74f15913ecd1c
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2026-03-15 14:26:25 -0300

    chore: removed unused variables

--- tests/functional/transactional.spec.ts
+++ tests/functional/transactional.spec.ts
@@ -406,7 +406,6 @@ test.group('ADMIN or FINANCE get a transaction by id', (group) => {
 
   test('should return 403 when an normal user gets a client by id and his transactions', async ({
     client,
-    assert,
   }) => {
     const finance = await User.create({
       fullName: 'Finance User',
@@ -415,7 +414,7 @@ test.group('ADMIN or FINANCE get a transaction by id', (group) => {
       role: USER_ROLE.FINANCE,
     })
 
-    const token = await User.accessTokens.create(finance)
+    await User.accessTokens.create(finance)
 
     const client1 = await Client.create({
       name: 'Client 1',
@@ -477,7 +476,7 @@ test.group('ADMIN or FINANCE get a transaction by id', (group) => {
       role: USER_ROLE.FINANCE,
     })
 
-    const token = await User.accessTokens.create(finance)
+    await User.accessTokens.create(finance)
 
     const client1 = await Client.create({
       name: 'Client 1',
@@ -512,13 +511,6 @@ test.group('ADMIN or FINANCE get a transaction by id', (group) => {
     }
 
     const transaction = await client.post('/api/v1/transactions').json(transactionExample1)
-
-    const user = await User.create({
-      fullName: 'Regular User',
-      email: 'user@example.com',
-      password: 'password',
-      role: USER_ROLE.USER,
-    })
 
     const response = await client.get(`/api/v1/transactions/${transaction.body().data.id}`)
 
